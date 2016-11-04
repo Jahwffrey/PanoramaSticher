49bf2f727da0f4f01309ba9158d71e39b5565801
Jahwffrey <johnthomason@umail.ucsb.edu>
2016-11-04 14:54:34 -0700

    Flow tracking

--- mosaic.cpp
+++ mosaic.cpp
@@ -13,8 +13,12 @@ using namespace cv;
 //eww global
 CvSize imgSize = {640,480};
 
+int ptMode = 0;
+
 int main(int argc,char** argv){
 	Mat feed;
+	Mat prevFeed;
+	vector<Point2f> prevFeatures;
 	VideoCapture cap;
 	FastFeatureDetector feat;
 
@@ -39,24 +43,52 @@ int main(int argc,char** argv){
 
 
 	feat = FastFeatureDetector();
+
 	while(true){
 		cap >> feed;
 		resize(feed,feed,imgSize);
-		vector<KeyPoint> features;
-		feat.detect(feed,features); 
+		Mat frame = feed.clone();
 
+		vector<KeyPoint> features;
+		vector<Point2f> feats;
+		feat.detect(frame,features); 
 		for(int i = 0;i < features.size();i++){
 			//Vec3b pixel = feed.at<Vec3b>(features[i].pt.y,features[i].pt.x);
 			Vec3b pixel = Vec3b(0,255,255);
-			feed.at<Vec3b>(features[i].pt.y,features[i].pt.x) = pixel;
-			feed.at<Vec3b>(features[i].pt.y+1,features[i].pt.x) = pixel;
-			feed.at<Vec3b>(features[i].pt.y-1,features[i].pt.x) = pixel;
-			feed.at<Vec3b>(features[i].pt.y,features[i].pt.x+1) = pixel;
-			feed.at<Vec3b>(features[i].pt.y,features[i].pt.x-1) = pixel;
+			if(ptMode == 1){
+				frame.at<Vec3b>(features[i].pt.y,features[i].pt.x) = pixel;
+				frame.at<Vec3b>(features[i].pt.y+1,features[i].pt.x) = pixel;
+				frame.at<Vec3b>(features[i].pt.y-1,features[i].pt.x) = pixel;
+				frame.at<Vec3b>(features[i].pt.y,features[i].pt.x+1) = pixel;
+				frame.at<Vec3b>(features[i].pt.y,features[i].pt.x-1) = pixel;
+			}
+			feats.push_back(Point2f(features[i].pt.x,features[i].pt.y));
+		}
+		if(!prevFeed.empty() && prevFeatures.size() > 0){
+			vector<Point2f> validFeatures;
+			vector<unsigned char> status;
+			vector<float> err;
+			//calcOpticalFlowPyrLK(prevFeed,feed,prevFeatures,feats,status,err);
+			calcOpticalFlowPyrLK(prevFeed,feed,prevFeatures,validFeatures,status,err);
+		
+			for(int i = 0;i < status.size();i++){
+				if(status[i] == 1){
+					if(ptMode == 2){
+						line(frame,prevFeatures[i],validFeatures[i],Scalar(0,255,255));
+					}
+				}
+			}
 		}	
 
-		imshow("Whaddap",feed);
+		prevFeatures = feats;
+		prevFeed = feed.clone(); 
+		
+		imshow("Whaddap",frame);
 
+		int key = waitKey(10);
+		if(key != -1){
+			if(key == 102) ptMode = (ptMode + 1) % 3;
+		}
 	}
 
 	return 0;
